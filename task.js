87cb6886ed981cb11333a53c6cf226984202876f
Christina Merlin <81285621+Christinamerlin@users.noreply.github.com>
2024-08-22 11:15:37 +0530

    Update task.js

--- task.js
+++ task.js
@@ -3,9 +3,22 @@ fetch("https://fakestoreapi.com/products")
 .then(data=>{
     const jpdt=data.filter(pdt=>pdt.category==="jewelery");
     jpdt.sort((a,b)=>b.price-a.price);
-    console.log(jpdt);
-})
+    
 
+    const pdtlist=document.getElementById("pdtlist");
+    jpdt.forEach(pdt=>{
+        const pdtdiv=document.createElement("div");
+        pdtdiv.style.width='50%';
+        pdtdiv.style.height='500px';
+        pdtdiv.style.float='left';
+        pdtdiv.innerHTML=
+        "<h3> ID: "+pdt.id+"</h3>"+
+        "<p> <b>Title:</b> "+pdt.title+"</p>"+"<p> <b>Price:</b> "+pdt.price+"</p>"+"<p>"+pdt.description+"</p>"
+        +"<p><b>Category:</b> "+pdt.category+"</p>"+'<img src="'+pdt.image+'"width="100">'+"</img>"+"<p> <b>Rating: </b>"+pdt.rating.rate+"</p>";
+    
+    pdtlist.appendChild(pdtdiv);
+    });
+})
 
 
 
